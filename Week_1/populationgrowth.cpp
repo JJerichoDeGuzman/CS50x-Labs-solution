f4dc43060e322d856ed23638642cdbf1283e5951
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-10 21:23:34 +0800

    ..

---
+++ Week_1/populationgrowth.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(){
+
+
+// Starting population: User input at least 9 llamas 
+
+    std::cout 
+
+
+//Ending population: At least equal to user input
+
+
+//Calculate the number of years required
+
+
+// Print the the final answer
+
+
+
+    return 0; 
+
+}
