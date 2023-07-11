aeefa6b784d87f809ef3921aa7a4b10168196f14
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-11 22:01:10 +0800

    LLamas solving

--- Week_1/populationgrowth.cpp
+++ Week_1/populationgrowth.cpp
@@ -5,10 +5,24 @@ int main(){
 
 // Starting population: User input at least 9 llamas 
 
-    std::cout 
-
+    std::cout << "Enter the starting Llama population: " << std::endl;
+    int startPopu {};
+    std::cin >> startPopu; 
+    while (startPopu < 9 ) {
+         std::cout << "Enter at least 9 Llamas: " << std::endl;
+         std::cin >> startPopu; 
+    }
 
 //Ending population: At least equal to user input
+
+    std::cout << "Enter the desired ending population of Llamas: " << std::endl;
+    int endPopu {}; 
+    std::cin >> endPopu;
+    while (endPopu < startPopu ) {
+         std::cout << "Enter at least: " << startPopu << " Llamas" << std::endl;  
+         std::cin >> endPopu;
+
+    }
 
 
 //Calculate the number of years required
