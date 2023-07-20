a74cb87e78b48b0b130d8f61a973be8cb636254a
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-20 13:09:45 +0800

    .,

--- Week_1/Upside_down_Triangle1.cpp
+++ Week_1/Upside_down_Triangle1.cpp
@@ -3,8 +3,8 @@
 int main (){
     int n {5};
     
-    for (int i = 5; i >= 0; --i){
-        for (int j = 1; j >= i; ++j)
+    for (int i = 1; i >= n; ++i){
+        for (int j = n; j >= i; --j)
         {
             std::cout << "#" ;
         }
