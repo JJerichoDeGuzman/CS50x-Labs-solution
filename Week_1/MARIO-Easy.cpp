5f7f8f75bc8646f0945b1ebf93e099c9ed2fb502
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-22 14:42:39 +0800

    Triangle patterns solved

--- Week_1/MARIO-Easy.cpp
+++ Week_1/MARIO-Easy.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 int main() {
-    int n = 5;
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= i; ++j) {
+    int n {5};
+    for (int i = n; i >= 1; --i) {
+        for (int j = n; j > i; --j) {
+            std::cout << "-";
+        }
+        for (int k = 1; k <= i; ++k) {
             std::cout << "#";
         }
         std::cout << std::endl;
